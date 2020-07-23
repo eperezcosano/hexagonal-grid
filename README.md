6cd2e361f76dc0ca28acbccf8de9d888dce524a9
Izan Pérez Cosano <izan.perez@estudiant.upc.edu>
2020-07-23 16:34:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+#How to draw a hex grid on HTML Canvas
+In this article we are going to learn how to get a perfect hexagon grid using JavaScript to draw on a HTML canvas. We first need to know a bit of trigonometry to solve this problem as it is necessary for all the calculations for the coordinate points composing a regular polygon.
---
+++ main.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const a = 2 * Math.PI / 6;
+const r = 50;
+
+function init() {
+  drawGrid(canvas.width, canvas.height);
+}
+init();
+
+function drawGrid(width, height) {
+  for (let y = r; y + r * Math.sin(a) < height; y += r * Math.sin(a)) {
+    for (let x = r, j = 0; x + r * (1 + Math.cos(a)) < width; x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)) {
+      drawHexagon(x, y);
+    }
+  }
+}
+
+function drawHexagon(x, y) {
+  ctx.beginPath();
+  for (let i = 0; i < 6; i++) {
+    ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
